6890bd94c3ffd7eaef621e8faecd8fbe75a7dbed
Roshan Kern <roshan.kern@ucdenver.edu>
2025-08-13 17:43:01 -0600

    use smaller images

--- src/pages/MkpLoves.tsx
+++ src/pages/MkpLoves.tsx
@@ -1,7 +1,7 @@
 import { ArrowLeft } from 'lucide-react';
 import { useNavigate } from 'react-router-dom';
 import { Card, CardContent } from '@/components/ui/card';
-import teaAndTreatsImage from '@/assets/favorites/tea_and_treats.png';
+import teaAndTreatsImage from '@/assets/favorites/tea_and_treats.jpg';
 import paulineImage from '@/assets/favorites/IMG_2280.jpg';
 import familyImage from '@/assets/favorites/32C267AA-736F-452F-A9B1-F6DC0D58E48C.jpg';
 
